c29ed8f00e0bb093a130ebeb944c616c44c2be47
lucasdiniz1708 <lucasmoraes1708@gmail.com>
2023-05-18 02:34:40 -0300

    Colocando Cliente1 e 2 Formato Jason

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,14 +1,32 @@
 package com.algaworks.algalog.api.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algalog.domain.model.Cliente;
 
 @RestController
 public class ClienteController {
 	
 	@GetMapping("/clientes")
-	public String listar() {
-		return "Teste";
+	public List<Cliente> listar() {
+		
+		var cliente1 = new Cliente();
+		cliente1.setId(1L);
+		cliente1.setNome("João");
+		cliente1.setEmail("joao@email");
+		cliente1.setTelefone("31 993631810");
+		
+		var cliente2 = new Cliente();
+		cliente2.setId(2L);
+		cliente2.setNome("Maria");
+		cliente2.setEmail("Maria@email");
+		cliente2.setTelefone("31 993631810");
+
+		return Arrays.asList(cliente1,cliente2);
 	}
 
 }
