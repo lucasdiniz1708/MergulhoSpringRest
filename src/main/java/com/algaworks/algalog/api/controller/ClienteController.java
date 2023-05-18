fef6bdfe93d1c986a1c8d62dcd791869889de364
lucasdiniz1708 <lucasmoraes1708@gmail.com>
2023-05-18 03:49:58 -0300

    Banco de dados implementado

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.algaworks.algalog.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,22 +14,14 @@ import com.algaworks.algalog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João3");
-		cliente1.setEmail("joao@email");
-		cliente1.setTelefone("31 993631810");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setEmail("Maria@email");
-		cliente2.setTelefone("31 993631810");
-
-		return Arrays.asList(cliente1,cliente2);
+		return manager.createQuery("from Cliente",Cliente.class)
+				.getResultList();
 	}
 
 }
